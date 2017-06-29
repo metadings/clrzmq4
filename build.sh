20e580c0a038af6b720e59db991dd7e5cea425c0
Simon Giesecke <simon.giesecke@btc-ag.com>
2017-06-29 18:24:07 +0200

    Added monocov build

--- build.sh
+++ build.sh
@@ -36,6 +36,19 @@ fi
 nuget install NUnit.ConsoleRunner -Version 3.6.1 -OutputDirectory testrunner
 nuget install coveralls.net -Version 0.7.0 -OutputDirectory tools
 
+pushd
+curl -sS https://api.nuget.org/packages/mono.cecil.0.9.5.4.nupkg > /tmp/mono.cecil.0.9.5.4.nupkg.zip
+unzip /tmp/mono.cecil.0.9.5.4.nupkg.zip -d /tmp/cecil
+cp /tmp/cecil/lib/net40/Mono.Cecil.dll .
+cp /tmp/cecil/lib/net40/Mono.Cecil.dll /tmp/cecil/
+git clone --depth=50 git://github.com/csMACnz/monocov.git ../../csMACnz/monocov
+cd ../../csMACnz/monocov
+cp /tmp/cecil/Mono.Cecil.dll .
+./configure
+make
+sudo make install
+popd
+
 xbuild /p:Configuration=Release clrzmq4.mono.sln
 
 export MONO_TRACE_LISTENER=Console.Out
