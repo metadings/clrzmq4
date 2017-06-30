51332dae80aead6226b14a8d3c2106c52aafd658
Simon Giesecke <simon.giesecke@btc-ag.com>
2017-06-30 11:46:59 +0200

    Set LD_LIBRARY_PATH

--- build.sh
+++ build.sh
@@ -59,6 +59,8 @@ xbuild /p:Configuration=Debug clrzmq4.mono.sln
 export MONO_TRACE_LISTENER=Console.Out
 
 COVFILE=$(pwd)/ZeroMQ.cov
+# ensure /usr/local/lib is in the library path, this is where the monocov instrumentation library is placed
+LD_LIBRARY_PATH=/usr/local/lib:${LD_LIBRARY_PATH}
 mono --debug --profile=monocov:outfile=${COVFILE},+[ZeroMQ] ./testrunner/NUnit.ConsoleRunner.3.6.1/tools/nunit3-console.exe ./ZeroMQTest/bin/Debug/ZeroMQTest.dll
 
 monocov --export-xml=ZeroMQ.cov.xml ${COVFILE}
